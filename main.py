690fab4a1a7ddd8f5499dd364e555c82b55e75f9
Abhai Kollara Dilip <abhai.dilip@gmail.com>
2018-04-17 22:24:33 +0530

    Handle base64

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import models
 import argparse
 import tensorflow as tf
+import base64
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Up-Scales an image using Image Super Resolution Model")
     parser.add_argument("imgpath", type=str, nargs="+", help="Path to input image")
     args = parser.parse_args()
-
     paths = args.imgpath
     return paths
 
 def super_resolve(path):
     with tf.device('/CPU:0'):
-        scale_factor = 2
-        model = models.DistilledResNetSR(scale_factor)
+        model = models.DistilledResNetSR(scale_factor=2)
         model.upscale(path, save_intermediate=True, mode="patch", patch_size=8, suffix="scaled")
+
+
+def web_request(base64_img):
+    with open('input_image', 'wb') as f:
+        f.write(base64.decodebytes(base64_img))
+
+    super_resolve("input_image")
+
+    with open("input_image_scaled(2x)", "rb") as f:
+        encoded_string = base64.b64encode(f.read())
+
+    return encoded_string
 
 
 if __name__ == "__main__":
